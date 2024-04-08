0fcff196a19e444dd560b13316d03218b6572532
gaoxiu <gaoxiu333@gmail.com>
2024-04-08 10:51:14 +0800

    won workspace

---
+++ packages/jslib/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+
+export default {
+    build: {
+        lib: { // 构建库
+            entry: resolve(__dirname, 'src/main.ts'),
+            formats: ['es', 'umd', 'cjs', 'iife'],
+            name:'jslib', // 存在um或者iife时，需要name作为全局变量的名字
+            fileName:'main' // 生成文件名称，默认package.json中的name
+        }
+    },
+    resolve: { alias: { src: resolve('src/') } },
+    plugins: [dts()],
+}
