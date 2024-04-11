96d217c91293991c70014bcb26ecc4c633ec81fa
gaoxiu <gaoxiu333@gmail.com>
2024-04-11 22:58:54 +0800

    添加 TODO

--- crt/src/index.ts
+++ crt/src/index.ts
@@ -5,7 +5,7 @@ const cwd = process.cwd()
 console.log('cwd', process.env.npm_config_user_agent)
 // const pkg
 spawn.sync('pnpm', ['add', 'lodash'], { stdio: 'inherit' })
-
+// TODO: 检查package中是否存在，如果已经存在了怎么办？
 
 function pkgFromUserAgent(userAgent: string | undefined) {
     if (!userAgent) return undefined
