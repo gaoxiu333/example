df095e369c022f49cea045ee080f43c6136b767c
gaoxiu <gaoxiu333@gmail.com>
2024-04-12 22:16:33 +0800

    添加 模版

--- crt/dist/index.js
+++ crt/dist/index.js
@@ -1,8 +1,21 @@
 import spawn from "cross-spawn";
 const cwd = process.cwd();
-console.log('cwd', process.env.npm_config_user_agent);
+console.log('start');
+const PKGS = [
+    {
+        name: 'eslint',
+        args: ['-D', '--save-exact']
+    },
+    {
+        name: 'prettier',
+        args: ['-D', '--save-exact']
+    }
+];
+const INIT = [];
+const allCommand = PKGS.map(Object.values).flat(2);
+console.log('cwd', allCommand);
 // const pkg
-spawn.sync('pnpm', ['add', 'lodash'], { stdio: 'inherit' });
+// TODO: 检查package中是否存在，如果已经存在了怎么办？
 function pkgFromUserAgent(userAgent) {
     if (!userAgent)
         return undefined;
@@ -16,5 +29,6 @@ function pkgFromUserAgent(userAgent) {
 const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent);
 const pkgManager = pkgInfo ? pkgInfo.name : 'npm';
 const isYarn1 = pkgManager === 'yarn' && pkgInfo?.version.startsWith('1.');
+spawn.sync(pkgManager, ['add', ...allCommand], { stdio: 'inherit' });
 console.log('pkgManger', pkgManager);
 //# sourceMappingURL=index.js.map
--- crt/src/index.ts
+++ crt/src/index.ts
@@ -1,10 +1,25 @@
 import spawn from "cross-spawn";
 
 const cwd = process.cwd()
+console.log('start')
 
-console.log('cwd', process.env.npm_config_user_agent)
+const PKGS = [
+    {
+        name: 'eslint',
+        args: ['-D', '--save-exact']
+    },
+    {
+        name: 'prettier',
+        args: ['-D', '--save-exact']
+    }
+]
+
+const INIT = [
+
+]
+const allCommand = PKGS.map(Object.values).flat(2)
+console.log('cwd', allCommand)
 // const pkg
-spawn.sync('pnpm', ['add', 'lodash'], { stdio: 'inherit' })
 // TODO: 检查package中是否存在，如果已经存在了怎么办？
 
 function pkgFromUserAgent(userAgent: string | undefined) {
@@ -20,5 +35,6 @@ function pkgFromUserAgent(userAgent: string | undefined) {
 const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent)
 const pkgManager = pkgInfo ? pkgInfo.name : 'npm'
 const isYarn1 = pkgManager === 'yarn' && pkgInfo?.version.startsWith('1.')
+spawn.sync(pkgManager, ['add', ...allCommand], { stdio: 'inherit' })
 
 console.log('pkgManger', pkgManager)
