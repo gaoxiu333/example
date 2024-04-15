f3572a8094ec728f5313ad0b5a74f30426f94987
gaoxiu <gaoxiu333@gmail.com>
2024-04-15 09:06:13 +0800

    feat: init tailwindcss

---
+++ apps/rigid-react/.prettierrc.mjs
@@ -0,0 +1,8 @@
+import prettierPluginTailwindcss from "prettier-plugin-tailwindcss";
+
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: [prettierPluginTailwindcss],
+};
+
+export default config;
--- apps/rigid-react/src/main.tsx
+++ apps/rigid-react/src/main.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>123</h1>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+    <h1>11</h1>
   </React.StrictMode>,
-)
+);
