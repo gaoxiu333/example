0cc2240cedf9beb8106242c9684e028558ef0d35
gaoxiu <gaoxiu333@gmail.com>
2024-04-15 09:16:31 +0800

    init shadcn ui

--- apps/rigid-react/src/main.tsx
+++ apps/rigid-react/src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Button } from "./components/ui/button";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
-    <h1>11</h1>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1> <h1>11</h1>
+    <Button>test</Button>
   </React.StrictMode>,
 );
