46b07d6c385b169b0ce064eb4ae8e931ef35348c
gaoxiu <gaoxiu333@gmail.com>
2024-04-16 21:12:16 +0800

    ts

--- apps/rigid-react/src/_Test/index.tsx
+++ apps/rigid-react/src/_Test/index.tsx
@@ -1,3 +1,22 @@
-export function Page() {
-  return <></>;
+import { useState } from "react";
+
+/**
+ *
+ * props 类型
+ */
+export function Page(props: {
+  children: React.ReactNode;
+  childrenElement?: React.JSX.Element;
+  styles?: React.CSSProperties;
+  onChange?: React.FormEventHandler<HTMLInputElement>;
+}) {
+  return <>{props.children}</>;
 }
+
+// function Types
+const App1: React.FC = () => {
+  const [value, setValue] = useState<string | null>(null);
+  return <></>;
+};
+
+export { App1 };
