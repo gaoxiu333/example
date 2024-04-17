6d4f34ae830b52ab24b9fc0a4624deb8b28ed26d
gaoxiu <gaoxiu333@gmail.com>
2024-04-17 21:40:16 +0800

    feat: TODO MVC

---
+++ apps/react-todomvc/src/App/NewTodoInput/index.tsx
@@ -0,0 +1,7 @@
+import { Layout } from "../styles";
+
+const NewTodoTextInput: React.FC = () => {
+  return <Layout></Layout>;
+};
+
+export { NewTodoTextInput };
---
+++ apps/react-todomvc/src/App/index.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "./styles";
+
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <section>
+        <h1>TodoMVC</h1>
+      </section>
+    </Layout>
+  );
+};
+export { App };
